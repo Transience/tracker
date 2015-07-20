31b88866a17b4dd18300eceb2d78b594e0331ec3
Transience <akhil.vkl@gmail.com>
2015-07-20 19:12:12 -0400

    shows trajectories

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from numpy import loadtxt
 homographyFilename = "laurier-homography.txt"
 homography = inv(loadtxt(homographyFilename))
 databaseFilename = "laurier.sqlite"
-trajectoryType = "object"
-objects = storage.loadTrajectoriesFromSqlite(databaseFilename, trajectoryType)
+objects = storage.loadTrajectoriesFromSqlite(databaseFilename, "object")
 features = storage.loadTrajectoriesFromSqlite(databaseFilename, "feature")
 
 drawing = False   # true if mouse is pressed
-cArray = []
+cArray = []   # stores new trajectory positions (temporary)
 fNo = 0   # stores the frame number
+tArray = []   # stores old trajectory positions
 
 def findObject(frameNum, x=0, y=0):
     box = []
@@ -38,7 +38,20 @@ def findObject(frameNum, x=0, y=0):
                 box.append([ymax, ymin, xmax, xmin])
             if xmax > x > xmin and ymax > y > ymin:
                     print "object detected: " + format(obj.getNum())
+                    print "object position: " + format(obj.getPositionAtInstant(frameNum).project(homography))
     return box
+
+def drawTrajectory(frame, frameNum):
+    global objects, tArray
+    for obj in objects:
+        if obj.existsAtInstant(frameNum):
+            prevPosition = obj.getPositionAtInstant(obj.getFirstInstant()).project(homography)
+            for pos in obj.getPositions():
+                position = pos.project(homography)
+                cv2.line(frame, (position[0], position[1]), (prevPosition[0], prevPosition[1]), (0, 0, 255), 2)
+                prevPosition = position
+                if pos[0] == obj.getPositionAtInstant(frameNum)[0] and pos[1] == obj.getPositionAtInstant(frameNum)[1]:
+                    break
 
 def drawBox(frame, frameNum):
     box = findObject(frameNum)
@@ -72,6 +85,7 @@ height = cap.get(4)
 while(cap.isOpened()):
     ret, frame = cap.read()
     drawBox(frame, fNo)
+    drawTrajectory(frame, fNo)
     for i in range(len(cArray)):
         cv2.circle(frame, (cArray[i][0], cArray[i][1]), 3, (0, 255, 0), -1)
     cv2.imshow('Video', frame)
