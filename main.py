5490dd8146d34cfce834f03bcf2c9deb668c236a
Transience <akhil.vkl@gmail.com>
2015-07-27 15:24:06 -0400

    partial editing while tracing

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ __author__ = 'Akhil'
 
 import cv2
 import storage
+import sqlite3
 from numpy.linalg.linalg import inv
 from numpy import loadtxt
 
 homographyFilename = "laurier-homography.txt"
 homography = inv(loadtxt(homographyFilename))
 databaseFilename = "laurier.sqlite"
+newFilename = "corrected.sqlite"
 objects = storage.loadTrajectoriesFromSqlite(databaseFilename, "object")
 features = storage.loadTrajectoriesFromSqlite(databaseFilename, "feature")
 
@@ -17,7 +19,6 @@ fNo = 0   # stores the frame number
 tArray = []   # stores old trajectory positions
 objTag = None   # stores selected object's id
 edit = False   # turns on edit mode if true
-dEdit = False   # flag for editing database
 
 def findObject(frameNum, x=None, y=None):
     global objects, features, objTag
@@ -78,7 +79,7 @@ def drawEditBox(frame):
         cv2.putText(frame,"toggle edit (e)", (width-125, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255))
 
 def coordinates(event, x, y, flags, param):
-    global drawing, cArray, fNo
+    global drawing, cArray, fNo, objTag
     if event == cv2.EVENT_LBUTTONDOWN:
         print x, y
         drawing = True
@@ -91,7 +92,39 @@ def coordinates(event, x, y, flags, param):
                 print "editing object: " + format(objTag) + " (" + format(x) + " ," + format(y) + ")"
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
+        if objTag is not None and edit == True:
+            tracing()
         del cArray[:]
+
+def tracing():
+    global objTag
+    try:
+        connection = sqlite3.connect(databaseFilename)
+        newConnection = sqlite3.connect(newFilename)
+        cursor = connection.cursor()
+        newCursor = newConnection.cursor()
+        cursor.execute("SELECT * from objects where object_id = " + format(objTag) + ";")
+        objData = cursor.fetchone()
+        newCursor.execute("insert or replace into objects (object_id, road_user_type, n_objects) values (?, ?, ?);", objData)
+        newConnection.commit()
+        connection.close()
+        newConnection.close()
+    except sqlite3.Error, e:
+        print "Error %s:" % e.args[0]
+
+
+def createDatabase():
+    try:
+        connection = sqlite3.connect(newFilename)
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS objects (object_id INTEGER, road_user_type INTEGER DEFAULT 0, n_objects INTEGER DEFAULT 1, PRIMARY KEY(object_id))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS objects_features (object_id INTEGER, trajectory_id INTEGER, PRIMARY KEY(object_id, trajectory_id))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS positions (trajectory_id INTEGER,frame_number INTEGER, x_coordinate REAL, y_coordinate REAL, PRIMARY KEY(trajectory_id, frame_number))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS velocities (trajectory_id INTEGER,frame_number INTEGER, x_coordinate REAL, y_coordinate REAL, PRIMARY KEY(trajectory_id, frame_number))")
+        connection.close()
+    except sqlite3.Error, e:
+        print "Error %s:" % e.args[0]
+
 
 cap = cv2.VideoCapture('laurier.avi')
 cv2.namedWindow('Video')
@@ -106,9 +139,12 @@ while(cap.isOpened()):
     drawEditBox(frame)
     k = cv2.waitKey(33) & 0xFF
     if k == 27:
+        tracing()
         break
     if k == 101:
         edit = edit != True
+        if edit == True:
+            createDatabase()
     for i in range(len(cArray)):
         cv2.circle(frame, (cArray[i][0], cArray[i][1]), 3, (0, 255, 0), -1)
     cv2.imshow('Video', frame)
